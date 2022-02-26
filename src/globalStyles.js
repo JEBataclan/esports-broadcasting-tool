7e04d2f2f6ed322378e94c63a805680e79ed70df
Jan Erickson Bataclan <jebataclan03@gmail.com>
2022-02-26 21:45:07 +0800

    Change font to Anton

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
+import Anton from "./fonts/Anton-Regular.ttf";
+
 const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'Anton';
+    src: url(${Anton}) format('truetype');
+}
+
 * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: "Montserrat", sans-serif;
+    font-family: "Anton";
 }
 `;
 
